d0ea4aefec219da1f42cc536097da891f023de9f
WesleyCardoso42 <wesleycards42@gmail.com>
2023-01-01 03:40:39 -0300

    Commit de segurança

--- components/formulario.php
+++ components/formulario.php
@@ -1,24 +1,35 @@
-<form action="/enviar.php" method="post" class="form-cadastro">
+<form action="/enviar.php" method="post" class="form-cadastro" name="form-cadastro">
+
     <div class="margem">
+
         <div class="form-cabecalho">
             <h3>Comece seus 10 dias de teste grátis do RD Station Marketing!</h3>
             <p>Não precisa cadastrar cartão de crédito. 😉</p>
         </div>
         <hr class="divisoria">
+
+        <!-- INPUT NOME -->
         <div class="input-text">
             <label for="nome">Diga, qual seu nome?</label>
-            <input type="text" name="nome" placeholder="Insira seu nome" required>
+            <input type="text" name="nome" id="nome" placeholder="Insira seu nome" required>
         </div>
+
+        <!-- INPUT EMAIL -->
         <div class="input-text">
             <label for="email">Seu email de trabalho</label>
-            <input type="text" name="email" placeholder="Insira seu email" required>
+            <input type="text" name="email" id="email" placeholder="Insira seu email" required>
+            <span class="span-email"></span>
         </div>
+
+        <!-- INPUT TELEFONE -->
         <div class="input-text">
             <label for="telefone">Seu telefone</label>
             <input type="text" name="telefone" id="telefone" placeholder="Insira seu número de telefone com DDD" maxlength="15" required/>
         </div>
+
+        <!-- INPUT OCUPACAO -->
         <div class="input-text">
-            <label for="ocupacao">Seu cargo de ocupação</label>
+            <label for="ocupacao" id="ocupacao">Seu cargo de ocupação</label>
             <!--<input type="text" name="cargo" placeholder="Selecione seu cargo" required> -->
             <select name="cargo" required>
                 <option value="Selecione">Selecione seu cargo</option>
@@ -31,29 +42,33 @@
             </select>
         </div>
 
-
+        <!-- INPUT SENHA -->
         <div class="input-text toggle-senha">
             <label for="senha">Crie uma senha</label>
             <input class="input-senha" type="password" name="senha" id="senha" placeholder="••••••••" required>
             <img class="password" src="/img/eye-open.svg" alt="">
         </div>
 
+        <!-- INPUT CONFIRMA SENHA -->
         <div class="input-text toggle-senha">
             <label for="senha">Confirme sua senha</label>
             <input class="input-senha" type="password" name="senha" id="confirma-senha" placeholder="Branding-rd@2021*" required>
             <img class="password" src="/img/eye-open.svg" alt="">
         </div>
-
         <span id="valida-senhas"></span>
 
+        <!-- INFORMACAO -->
         <div class="site-empresa">
             <p>Qual o site da sua empresa?</p>
         </div>
+
+        <!-- INPUT SITE RADIO -->
         <div class="site-radio">
             <input type="radio" name="site" id="checkbox-1" checked required>
             <label for="checkbox-1">Meu site é</label>
         </div>
         <input type="text" name="endereco_site" class="endereco-site" placeholder="Insira o endereço do seu site" required>
+        <span id="valida-url"></span>
         <div class="site-radio">
             <input type="radio" name="site" id="checkbox-2" required>
             <label for="checkbox-2">Ainda não tenho site</label>
@@ -72,6 +87,6 @@
         </ul>
     </div>
     <div class="button">
-        <button type="submit"><img src="img/Mobile-button.png" alt=""></button>
+        <button type="submit" id="enviar-form"><img src="img/Mobile-button.png" alt=""></button>
     </div>
 </form>
